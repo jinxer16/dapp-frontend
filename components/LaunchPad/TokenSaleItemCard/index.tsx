e9f1ffa9145b6fb5b5ea435db3436be74a823f81
Kingsley Victor <javaprodigy56@gmail.com>
2022-11-11 13:13:53 +0100

    working on launchpad

--- components/LaunchPad/TokenSaleItemCard/index.tsx
+++ components/LaunchPad/TokenSaleItemCard/index.tsx
@@ -1,32 +1,72 @@
 import { Fetcher, Token } from 'quasar-sdk-core';
-import React, { useEffect, useMemo, useState } from 'react';
-import { FiHeart, FiBell } from 'react-icons/fi';
-import { formatEther } from '@ethersproject/units';
+import React, { MouseEventHandler, useEffect, useMemo, useState } from 'react';
+import Countdown from 'react-countdown';
+import { formatEther, formatUnits } from '@ethersproject/units';
+import _ from 'lodash';
+import millify from 'millify';
 import { TokenSaleItemModel } from '../../../api/models/launchpad';
 import { useAPIContext } from '../../../contexts/api';
 import { useWeb3Context } from '../../../contexts/web3';
 import chains from '../../../assets/chains.json';
+import saleCreators from '../../../assets/token_sales_creators.json';
+import { fetchSaleItemInfo } from '../../../hooks/launchpad';
+
+type ISaleItemCardProps = {
+  data: TokenSaleItemModel;
+  saleType: 'public' | 'private';
+  onClick: (val: TokenSaleItemModel) => void;
+};
+
+const Timer = ({ date, completionText }: any) => {
+  return (
+    <Countdown
+      date={date}
+      renderer={({ days, hours, minutes, seconds, completed }) => (
+        <>
+          {completed ? (
+            <span className="font-Montserrat text-white font-[700] uppercase">{completionText}</span>
+          ) : (
+            <span className="font-Montserrat text-white font-[700] text-[16px]">
+              {days} Day(s) : {hours} Hr(s) : {minutes} Min(s) : {seconds} Sec(s)
+            </span>
+          )}
+        </>
+      )}
+    />
+  );
+};
 
 export default function PresaleItemCard({
-  id,
-  token,
-  tokensForSale,
-  hardCap,
-  softCap,
-  presaleRate,
-  minContribution,
-  maxContribution,
-  startTime,
-  proceedsTo,
-  endTime,
-  admin,
-  rank
-}: TokenSaleItemModel) {
+  data: {
+    id,
+    token,
+    hardCap,
+    softCap,
+    minContribution,
+    maxContribution,
+    startTime,
+    endTime,
+    rank,
+    tokensForSale,
+    presaleRate,
+    proceedsTo,
+    admin,
+    chainId: chainid
+  },
+  saleType,
+  onClick
+}: ISaleItemCardProps) {
   const { tokensListingAsDictionary } = useAPIContext();
   const { chainId } = useWeb3Context();
   const [tokenObject, setTokenObject] = useState<Token>();
 
   const chain = useMemo(() => chains[chainId as unknown as keyof typeof chains], [chainId]);
+  const saleCreator = useMemo(
+    () =>
+      saleCreators[chainId as unknown as keyof typeof saleCreators]?.[saleType === 'private' ? 'privateTokenSaleCreator' : 'publicTokenSaleCreator'],
+    [chainId, saleType]
+  );
+  const { totalEtherRaised } = fetchSaleItemInfo(saleCreator, id);
 
   useEffect(() => {
     if (!!chainId) {
@@ -43,7 +83,27 @@ export default function PresaleItemCard({
 
   return (
     <>
-      <div className="flex w-80 h-auto bg-[#161525] m-1 p-5 rounded-lg lg:w-85">
+      <div
+        onClick={() =>
+          onClick({
+            id,
+            token,
+            hardCap,
+            softCap,
+            minContribution,
+            maxContribution,
+            startTime,
+            endTime,
+            rank,
+            tokensForSale,
+            presaleRate,
+            proceedsTo,
+            admin,
+            chainId: chainid
+          })
+        }
+        className="flex w-80 h-auto bg-[#161525] m-1 p-5 rounded-lg lg:w-85 cursor-pointer"
+      >
         <div className="w-full flex flex-col ">
           <div className="flex items-center justify-between h-10 w-full">
             <div className="flex">
@@ -66,54 +126,64 @@ export default function PresaleItemCard({
               </span>
             </div>
           </div>
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full gap-1">
             <h2 className="text-[20px] uppercase text-[#fff] font-[800] pt-2 font-Montserrat">{tokenObject?.name}</h2>
             <h3 className="flex items-center justify-between text-[1rem] text-[#fff] capitalize font-[700] pb-2 font-Montserrat">
               <span>Max Contribution:</span>
-              <span>{formatEther(maxContribution)}</span>
+              <span>
+                {millify(parseFloat(formatEther(maxContribution)))} {chain?.symbol}
+              </span>
+            </h3>
+            <h3 className="flex items-center justify-between text-[1rem] text-[#fff] capitalize font-[700] pb-2 font-Montserrat">
+              <span>Min Contribution:</span>
+              <span>
+                {millify(parseFloat(formatEther(minContribution)))} {chain?.symbol}
+              </span>
             </h3>
           </div>
           <div className="flex flex-col my-5">
-            <span className="text-[#fff] font-Montserrat font-[600] pb-[0.099rem]">Progress ({progress.toFixed(3)}%)</span>
+            <span className="text-[#fff] font-Montserrat font-[600] pb-[0.099rem]">
+              Progress ({_.multiply(parseInt(totalEtherRaised), 100) / parseInt(hardCap)}%)
+            </span>
             <div className="h-[8px] bg-[#1673B9]">
-              <div className="h-full bg-green-400" style={{ width: progress + '%' }} />
+              <div className="h-full bg-green-400" style={{ width: _.multiply(parseInt(totalEtherRaised), 100) / parseInt(hardCap) + '%' }} />
             </div>
             <div className="flex justify-between text-center pt-[0.099rem]">
               <span className="text-[#fff] font-bold font-Montserrat">0</span>
-              <span className="text-[#fff] font-bold font-Montserrat">300</span>
+              <span className="text-[#fff] font-bold font-Montserrat">{millify(parseFloat(formatEther(hardCap)), { precision: 4 })}</span>
             </div>
           </div>
           <div className="flex flex-col">
             <div className="flex justify-between mt-2">
               <span className="text-[#fff] font-bold font-Montserrat">Hard Cap:</span>
-              <span className="text-[#fff] font-bold font-Montserrat">{hardCap}</span>
+              <span className="text-[#fff] font-bold font-Montserrat">
+                {millify(parseFloat(formatEther(hardCap)), { precision: 4 })} {chain?.symbol}
+              </span>
             </div>
             <div className="flex justify-between  mt-2">
               <span className="text-[#fff] font-bold font-Montserrat">Soft Cap:</span>
-              <span className="text-[#fff] font-bold font-Montserrat">{softCap}</span>
+              <span className="text-[#fff] font-bold font-Montserrat">
+                {millify(parseFloat(formatEther(softCap)), { precision: 4 })} {chain?.symbol}
+              </span>
             </div>
             <div className="flex justify-between  mt-2">
-              <span className="text-[#fff] font-bold font-Montserrat">Liquidity:</span>
-              <span className="text-[#fff] font-bold font-Montserrat">{liquidity}</span>
-            </div>
-            <div className="flex justify-between  mt-2">
-              <span className="text-[#fff] font-bold font-Montserrat">Lock Time:</span>
-              <span className="text-[#fff] font-bold font-Montserrat">{lockTime}</span>
+              <span className="text-[#fff] font-bold font-Montserrat">
+                {tokenObject?.symbol} per {chain.symbol}:
+              </span>
+              <span className="text-[#fff] font-bold font-Montserrat">
+                {tokenObject && millify(parseFloat(formatUnits(presaleRate, tokenObject.decimals)), { precision: 4 })} {tokenObject?.symbol}
+              </span>
             </div>
           </div>
           <div className="flex ">
-            <div className="flex w-full justify-between items-center mt-5 border-t-[0.03rem] border-t-[#5B5B5B] pt-3">
+            <div className="flex w-full flex-col justify-center items-center mt-5 border-t-[0.03rem] border-t-[#5B5B5B] pt-3 gap-2">
               <div className="flex flex-col">
-                <span className="text-[whitesmoke] text-sm  font-Montserrat">Sales Ends In:</span>
-                <span className="text-[whitesmoke] text-sm tracking-[0.16rem]  font-Montserrat">00:56:12:45</span>
+                <span className="text-[whitesmoke] text-sm font-Montserrat">Sale Starts In:</span>
+                <Timer date={parseInt(startTime)} completionText="Started" />
               </div>
-              <div className="flex items-center justify-between text-[#fff] font-bold text-[1.5rem]">
-                <span className="pr-[8px]">
-                  <FiHeart />
-                </span>
-                <span className="bg-[#1673B9] rounded-[7px] p-[0.5rem]">
-                  <FiBell />
-                </span>
+              <div className="flex flex-col">
+                <span className="text-[whitesmoke] text-sm font-Montserrat">Sales Ends In:</span>
+                <Timer date={parseInt(endTime)} completionText="Ended" />
               </div>
             </div>
           </div>
--- routes/launchpad/Presales.tsx
+++ routes/launchpad/Presales.tsx
@@ -1,12 +1,22 @@
-import React, { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useCallback, useEffect, useMemo, useState } from 'react';
+import { Interface } from '@ethersproject/abi';
+import { isAddress } from '@ethersproject/address';
+import { Contract } from '@ethersproject/contracts';
+import { Web3Provider } from '@ethersproject/providers';
+import { formatEther, parseEther, parseUnits } from '@ethersproject/units';
+import { Fetcher, Token } from 'quasar-sdk-core';
 import _ from 'lodash';
-import { FiPlus, FiChevronDown } from 'react-icons/fi';
+import { FiPlus, FiChevronDown, FiArrowLeft, FiArrowRight } from 'react-icons/fi';
 import { ToastContainer, toast } from 'react-toastify';
+import { abi as saleCreatorAbi } from 'vefi-token-launchpad-staking/artifacts/contracts/TokenSaleCreator.sol/TokenSaleCreator.json';
+import { abi as erc20Abi } from 'vefi-token-launchpad-staking/artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json';
 import { TokenSaleItemCard } from '../../components/LaunchPad';
 import { useWeb3Context } from '../../contexts/web3';
 import chains from '../../assets/chains.json';
 import tokenSaleCreators from '../../assets/token_sales_creators.json';
 import { useAPIContext } from '../../contexts/api';
+import rpcCall from '../../api/rpc';
+import { TokenSaleItemModel } from '../../api/models/launchpad';
 
 enum Subroutes {
   ALL_ITEMS,
@@ -15,108 +25,7 @@ enum Subroutes {
   APPLY
 }
 
-const mockData: Array<{
-  logo?: string;
-  tagName: 'gold' | 'silver' | 'bronze';
-  tagColor?: any;
-  name: string;
-  maxContribution: string;
-  hardCap: string;
-  softCap: string;
-  liquidity: string;
-  lockTime: string;
-}> = [
-  {
-    logo: '/images/brise.png',
-    tagName: 'gold',
-    name: 'Wrapped Brise',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  },
-  {
-    logo: '/images/vefi.png',
-    tagName: 'gold',
-    name: 'Vefi Ecosystem Token',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  },
-  {
-    logo: '/images/phantom.svg',
-    tagName: 'gold',
-    name: 'Phantom Token',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  },
-  {
-    logo: '/images/phantom.svg',
-    tagName: 'gold',
-    name: 'Phantom Token',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  },
-  {
-    logo: '/images/phantom.svg',
-    tagName: 'gold',
-    name: 'Phantom Token',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  },
-  {
-    logo: '/images/vefi.png',
-    tagName: 'gold',
-    name: 'Vefi Ecosystem Token',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  },
-  {
-    logo: '/images/vefi.png',
-    tagName: 'gold',
-    name: 'Vefi Ecosystem Token',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  },
-  {
-    logo: '/images/vefi.png',
-    tagName: 'gold',
-    name: 'Vefi Ecosystem Token',
-    tagColor: 'bg-[#d4af37]',
-    maxContribution: '4000000 BRISE',
-    hardCap: '4000000 BRISE',
-    softCap: '2000000 BRISE',
-    liquidity: '7%',
-    lockTime: '30 Days'
-  }
-];
-
-const AllSalesRoute = () => {
+const AllSalesRoute = ({ onClick }: any) => {
   const { publicSaleItems, fetchPublicTokenSaleItems } = useAPIContext();
   const [page, setPage] = useState<number>(1);
 
@@ -124,28 +33,31 @@ const AllSalesRoute = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-3">
       <div className="flex flex-col md:flex-row justify-center items-center gap-2 flex-wrap">
-        {_.map(mockData, (data, index) => (
-          <TokenSaleItemCard
-            key={index}
-            logo={data.logo}
-            tagName={data.tagName}
-            tagColor={data.tagColor}
-            name={data.name}
-            maxContribution={data.maxContribution}
-            hardCap={data.hardCap}
-            softCap={data.softCap}
-            liquidity={data.liquidity}
-            lockTime={data.lockTime}
-            progress={Math.random() * 100}
-          />
+        {_.map(publicSaleItems.items, (data, index) => (
+          <TokenSaleItemCard key={index} data={data} saleType="public" onClick={(val) => onClick(val)} />
         ))}
+      </div>
+      <div className="flex justify-center items-center gap-2 text-white/70">
+        <button onClick={() => setPage((p) => p - 1)} disabled={page === 1} className="bg-transparent">
+          <FiArrowLeft />
+        </button>
+        <span>
+          Page {page} of {Math.ceil(publicSaleItems.totalItems / 20)}
+        </span>
+        <button onClick={() => setPage((p) => p + 1)} disabled={page >= Math.ceil(publicSaleItems.totalItems / 20)} className="bg-transparent">
+          <FiArrowRight />
+        </button>
       </div>
     </div>
   );
 };
 
+const SelectedSaleItemRoute = ({}: TokenSaleItemModel) => {
+  return <>Heya</>;
+};
+
 const CreateSaleRoute = () => {
-  const { chainId } = useWeb3Context();
+  const { chainId, library } = useWeb3Context();
   const [data, setData] = useState({
     token: '',
     tokensForSale: 0,
@@ -161,6 +73,36 @@ const CreateSaleRoute = () => {
   });
   const chain = useMemo(() => chains[chainId as unknown as keyof typeof chains], [chainId]);
   const publicSaleCreator = useMemo(() => tokenSaleCreators[chainId as unknown as keyof typeof tokenSaleCreators].publicTokenSaleCreator, [chainId]);
+  const [tk, setToken] = useState<Token>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const isValidForm = useMemo(
+    () =>
+      !!data.token &&
+      isAddress(data.token) &&
+      isAddress(data.admin) &&
+      isAddress(data.proceedsTo) &&
+      data.softCap > 0 &&
+      data.hardCap > 0 &&
+      data.presaleRate > 0 &&
+      data.maxContribution > 0 &&
+      data.minContribution > 0 &&
+      data.startTime > 0 &&
+      data.daysToLast > 0,
+    [
+      data.admin,
+      data.daysToLast,
+      data.hardCap,
+      data.maxContribution,
+      data.minContribution,
+      data.presaleRate,
+      data.proceedsTo,
+      data.softCap,
+      data.startTime,
+      data.token
+    ]
+  );
+  const [saleCreationFee, setSaleCreationFee] = useState<number>(0);
+  const [feePercentage, setFeePercentage] = useState<number>(0);
 
   const handleInputChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) =>
@@ -172,12 +114,106 @@ const CreateSaleRoute = () => {
     []
   );
 
+  const onSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      try {
+        e.preventDefault();
+        if (isValidForm && !!tk) {
+          setIsLoading(true);
+          const tokenAmount = parseUnits(data.tokensForSale.toPrecision(5), tk.decimals).toHexString();
+          const presaleRate = parseUnits(data.presaleRate.toPrecision(5), tk.decimals).toHexString();
+          const hardCap = parseEther(data.hardCap.toPrecision(7)).toHexString();
+          const softCap = parseEther(data.softCap.toPrecision(7)).toHexString();
+          const minContribution = parseEther(data.minContribution.toPrecision(7)).toHexString();
+          const maxContribution = parseEther(data.maxContribution.toPrecision(7)).toHexString();
+          const startTime = `0x${_.divide(data.startTime, 1000).toString(16)}`;
+
+          const provider = new Web3Provider(library?.givenProvider);
+          const tokenContract = new Contract(data.token, erc20Abi, provider.getSigner());
+          const approvalTx = await tokenContract.approve(publicSaleCreator, tokenAmount);
+          await approvalTx.wait();
+          toast('Approved!', { type: 'info' });
+
+          const saleCreatorContract = new Contract(publicSaleCreator, saleCreatorAbi, provider.getSigner());
+          const initTx = await saleCreatorContract.initTokenSale(
+            data.token,
+            tokenAmount,
+            hardCap,
+            softCap,
+            presaleRate,
+            minContribution,
+            maxContribution,
+            startTime,
+            data.daysToLast,
+            data.proceedsTo,
+            data.admin,
+            { value: parseEther(saleCreationFee.toPrecision(4)).toHexString() }
+          );
+
+          await initTx.wait();
+          toast('Created successfully', { type: 'success' });
+          setIsLoading(false);
+        }
+      } catch (error: any) {
+        setIsLoading(false);
+        toast(error.message, { type: 'error' });
+      }
+    },
+    [
+      data.admin,
+      data.daysToLast,
+      data.hardCap,
+      data.maxContribution,
+      data.minContribution,
+      data.presaleRate,
+      data.proceedsTo,
+      data.softCap,
+      data.startTime,
+      data.token,
+      data.tokensForSale,
+      isValidForm,
+      library?.givenProvider,
+      publicSaleCreator,
+      saleCreationFee,
+      tk
+    ]
+  );
+
+  useEffect(() => {
+    if (!!data.token && isAddress(data.token) && !!chain) {
+      (async () => {
+        try {
+          const token = await Fetcher.fetchTokenData(chainId || 97, data.token, chain.rpcUrl);
+          setToken(token);
+        } catch (error: any) {
+          console.log(error);
+        }
+      })();
+    }
+  }, [data.token, chain, chainId]);
+
+  useEffect(() => {
+    if (!!publicSaleCreator && !!chain) {
+      (async () => {
+        const saleAbiInterface = new Interface(saleCreatorAbi);
+        const data1 = saleAbiInterface.getSighash('saleCreationFee()');
+        const data2 = saleAbiInterface.getSighash('feePercentage()');
+
+        const fee = await rpcCall(chain.rpcUrl, { method: 'eth_call', params: [{ to: publicSaleCreator, data: data1 }, 'latest'] });
+        const percentage = await rpcCall(chain.rpcUrl, { method: 'eth_call', params: [{ to: publicSaleCreator, data: data2 }, 'latest'] });
+
+        setSaleCreationFee(parseFloat(formatEther(fee)));
+        setFeePercentage(parseInt(percentage));
+      })();
+    }
+  }, [chain, publicSaleCreator]);
+
   return (
     <div className="flex justify-center items-center mx-auto w-full flex-col md:flex-row px-2 py-2">
       <div className="card shadow-xl bg-[#000]/50 w-full md:w-1/2">
         <div className="card-body w-full overflow-auto">
           <span className="card-title font-Montserrat text-white/75">Create Presale Launch</span>
-          <form className="w-full flex flex-col gap-2">
+          <form onSubmit={onSubmit} className="w-full flex flex-col gap-2">
             <div className="flex flex-col justify-center gap-2">
               <label className="font-poppins text-white/60">Token*</label>
               <input
@@ -273,7 +309,6 @@ const CreateSaleRoute = () => {
                 placeholder="dd-mm-yyyy"
                 type="datetime-local"
                 className="outline-0 bg-[#000]/70 py-4 px-4 rounded-[12px] text-white flex-1"
-                min={new Date(Date.now() + 60 * 60 * 24 * 1000).toISOString().replace('Z', '')}
                 pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}"
                 name="startTime"
                 onChange={handleInputChange}
@@ -319,10 +354,16 @@ const CreateSaleRoute = () => {
               <span className="text-info text-[12px] font-poppins">Address with admin rights over this sale</span>
             </div>
             <div className="flex justify-between items-center w-full">
-              <span className="text-white/80 font-Montserrat text-[18px]">Network Fee</span>
-              <span className="text-white/80 font-Montserrat text-[18px]">{/* {networkFee} {chain.symbol} */}</span>
+              <span className="text-white/80 font-Montserrat text-[18px]">Fee</span>
+              <span className="text-white/80 font-Montserrat text-[18px]">
+                {saleCreationFee} {chain.symbol} (+ {feePercentage}% {chain.symbol} raised during sale)
+              </span>
             </div>
-            <button type="submit" className={`bg-[#0cedfc] btn py-[12px] px-[12px] rounded-[10px] w-full`}>
+            <button
+              type="submit"
+              disabled={!isValidForm || isLoading || !tk}
+              className={`bg-[#0cedfc] btn py-[12px] ${isLoading ? 'loading' : ''} px-[12px] rounded-[10px] w-full`}
+            >
               <span className="text-[#2b2828] font-[700] text-[15px]">Create Presale Launch</span>
             </button>
           </form>
@@ -337,6 +378,7 @@ const ApplyForPresaleRoute = () => <div>Apply For Presale</div>;
 
 export default function Presales() {
   const [selectedRoute, setSelectedRoute] = useState<Subroutes>(Subroutes.ALL_ITEMS);
+  const [selectedSaleItem, setSelectedSaleItem] = useState<TokenSaleItemModel>();
 
   return (
     <div className="h-full overflow-auto hidden-scrollbar">
@@ -459,9 +501,17 @@ export default function Presales() {
           </div>
         </div>
       </div>
-      {selectedRoute === Subroutes.ALL_ITEMS && <AllSalesRoute />}
+      {selectedRoute === Subroutes.ALL_ITEMS && (
+        <AllSalesRoute
+          onClick={(item: TokenSaleItemModel) => {
+            setSelectedSaleItem(item);
+            setSelectedRoute(Subroutes.SINGLE_ITEM);
+          }}
+        />
+      )}
       {selectedRoute === Subroutes.APPLY && <ApplyForPresaleRoute />}
       {selectedRoute === Subroutes.CREATE_NEW && <CreateSaleRoute />}
+      {selectedRoute === Subroutes.SINGLE_ITEM && <SelectedSaleItemRoute {...(selectedSaleItem as TokenSaleItemModel)} />}
     </div>
   );
 }
